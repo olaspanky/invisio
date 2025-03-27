6018895d8634c9fb20c5a4a8fa6e635ac4d3d94b
Olakareem <olakareemomobolarinwa@gmail.com>
2025-03-27 12:03:22 +0100

    first commit

--- src/app/components/DashboardLoader.tsx
+++ src/app/components/DashboardLoader.tsx
@@ -4,7 +4,7 @@ const DashboardSkeletonLoader = () => {
   return (
     <div className="2xl:p-6 p-3 rounded-lg shadow-md bg-gray-100 animate-pulse">
       <div className='flex flex-col h-screen w-full justify-center items-center gap-5'>
-             <div><h1 className='text-5xl font-extrabold animate-pulse text-primary font-custom'>Loading your Invisio&#8482; Dashboard</h1></div>
+             <div><h1 className='text-2xl font-extrabold animate-pulse text-primary font-custom'>Loading your Invisio&#8482; Dashboard</h1></div>
 
     </div>
     </div>
