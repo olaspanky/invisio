5d31ebb0ca6a35e63af5e25ba3c9175353e49369
Olakareem <olakareemomobolarinwa@gmail.com>
2025-04-16 15:09:10 +0100

    fix

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { FiSearch, FiUser } from "react-icons/fi";
-
+import Image from "next/image";
 interface CardProps {
   title: string;
   description: string;
+  icon: any; // Adjust the type of `icon` if you know its specific type
 }
 
-const Card: React.FC<CardProps> = ({ title, description }) => {
+const Card: React.FC<CardProps> = ({ title, description, icon }) => {
   return (
        <div className=" bg-white p-5 2xl:p-12 pr-7 2xl:pr-20 text-[#3D84ED] rounded-[20px] bg-contain bg-no-repeat bg-right"  style={{
                 backgroundImage: `url('/assets/boxes.png')`,
             }}> 
-      <h2 className=" font-semibold mb-2">{title}</h2>
-      <p className=" leading-6 2xl:leading-9 text-black">{description}</p>
+            <div className="flex  gap-2 ">
+              <Image
+                src={icon} alt="" className="w-5 h-5"  />
+            <h2 className="text-lg lg:text-xl font-semibold mb-2">{title}</h2>
+
+            </div>
+     
+      <p className="text-lg lg:text-xl leading-6 2xl:leading-9 text-black">{description}</p>
     </div>
   );
 };
--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -2,15 +2,20 @@
 import React from "react";
 import { FiSearch, FiUser } from "react-icons/fi";
 import Card from "./Card";  
-
+import i1 from "../../../public/assets/i1.svg";
+import i2 from "../../../public/assets/d1.svg";
+import i3 from "../../../public/assets/d2.svg";
+import i4 from "../../../public/assets/d3.svg";
+import i5 from "../../../public/assets/d4.svg";
+import i6 from "../../../public/assets/d5.svg";
 const Dashboard: React.FC = () => {
 
     const cardData = [
-        { title: "Disease Burden", description: "The disease burden analytics charts reports trend of diseases diagnosed by different physician specialities. This offers insight on flow of patients diagnosed by specialists and supports targeting of physicians for specific diseases of interest" },
-        { title: "HCP Treatment Mapping", description: "This analytics provides insight into doctors’ treatment preference for each disease diagnosed and treated and across diseases categories. All drugs prescribed are mapped to and standardized using global best practice of the WHO Anatomical Therapeutic Chemical (ATC) classification system, from levels 1 to 5" },
-        { title: "Prescription Analytics", description: "This analytics provides more specific insight into pattern of prescription for individual diseases or disease categories, with deeper insight into how doctors prescribe in branded or generic names and share of these for every disease. " },
-        { title: "Diagnostics", description: "This dashboard presents analytics of clinical diagnostic test requests from anonymized real world data, providing a wide, unprecedented view of diagnostic test requests across Nigeria, Africa’s most populous country. Individual data is processed only for purpose of analysis while maintaining form as gathered in real practice." },
-        { title: "Cost of Care Analytics", description: "The disease prevalence analytics charts reports trend of diseases diagnosed by different physician specialities. This offers insight on flow of patients diagnosed by specialists and supports targeting of physicians for specific diseases of interest" }
+        { title: "Disease Burden", icon: i2, description: "The disease burden analytics charts reports trend of diseases diagnosed by different physician specialities. This offers insight on flow of patients diagnosed by specialists and supports targeting of physicians for specific diseases of interest" },
+        { title: "HCP Treatment Mapping", icon: i3, description: "This analytics provides insight into doctors’ treatment preference for each disease diagnosed and treated and across diseases categories. All drugs prescribed are mapped to and standardized using global best practice of the WHO Anatomical Therapeutic Chemical (ATC) classification system, from levels 1 to 5" },
+        { title: "Prescription Analytics", icon: i4, description: "This analytics provides more specific insight into pattern of prescription for individual diseases or disease categories, with deeper insight into how doctors prescribe in branded or generic names and share of these for every disease. " },
+        { title: "Diagnostics", icon: i5, description: "This dashboard presents analytics of clinical diagnostic test requests from anonymized real world data, providing a wide, unprecedented view of diagnostic test requests across Nigeria, Africa’s most populous country. Individual data is processed only for purpose of analysis while maintaining form as gathered in real practice." },
+        { title: "Cost of Care Analytics", icon: i6, description: "The disease prevalence analytics charts reports trend of diseases diagnosed by different physician specialities. This offers insight on flow of patients diagnosed by specialists and supports targeting of physicians for specific diseases of interest" }
       ];
 
 
@@ -43,7 +48,7 @@ const Dashboard: React.FC = () => {
       {/* Cards Grid */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-12 2xl:gap-20">
         {cardData.map((card, index) => (
-          <Card key={index} title={card.title} description={card.description} />
+          <Card key={index} title={card.title} description={card.description} icon={card.icon} />
         ))}
       </div>
 
