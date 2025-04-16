f212da15f6dbbcb6e5c9674f62a6ebd9b222e969
Olakareem <olakareemomobolarinwa@gmail.com>
2025-04-16 13:02:03 +0100

    fix

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -10,7 +10,7 @@ const Card: React.FC<CardProps> = ({ title, description }) => {
   return (
     <div className="bg-white p-5 2xl:p-12 pr-7 2xl:pr-20 text-[#3D84ED] rounded-lg shadow-md hover:shadow-lg transition-shadow text-[18px]">
       <h2 className=" font-semibold mb-2">{title}</h2>
-      <p className=" leading-6 2xl:leading-9">{description}</p>
+      <p className=" leading-6 2xl:leading-9 text-black">{description}</p>
     </div>
   );
 };
